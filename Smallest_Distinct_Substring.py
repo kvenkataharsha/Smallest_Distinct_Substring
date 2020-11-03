df3bcf9c7a37dcc9bda82180d2f29cf8e752d6ae
k venkata harsha <kvenkataharsha@gmail.com>
2020-11-03 11:53:29 +0530

    Create Smallest_Distinct_Substring.py

---
+++ Smallest_Distinct_Substring.py
@@ -0,0 +1,34 @@
+from collections import defaultdict 
+MAX_CHARS = 256
+
+h = input()                                          #input     
+distinct_length = len(set([i for i in h]))           #lengthing all the disticnt characters in the input
+    
+currrent_length = defaultdict(lambda: 0)            #maintaing the current length and start
+length = 0
+start = 0
+minimum_length = len(h)
+    
+for i in range(len(h)):                               #Finding the substring contating all the distinct characters
+    currrent_length[h[i]] += 1
+        
+    if currrent_length[h[i]] == 1:                #Increasing the length when we see a disticnt character.
+        length += 1
+            
+    if length == distinct_length:                         #Removing the repated charaters in the substring we found, from the start 
+        while currrent_length[h[start]] > 1: 
+            if currrent_length[h[start]] > 1: 
+                currrent_length[h[start]] -= 1
+                    
+            start += 1                                 #and update the start index
+                 
+        substring_lenghth = i - start + 1                      #and update the substring size
+
+        if minimum_length > substring_lenghth:                        #and check if this substring is the minimum substring or not.if not we will upate the minimum lenghth
+            minimum_length = substring_lenghth 
+            currrent_length = start 
+        else:
+            current_length = start
+         
+answer = h[currrent_length: currrent_length + minimum_length]
+print(len(answer))
